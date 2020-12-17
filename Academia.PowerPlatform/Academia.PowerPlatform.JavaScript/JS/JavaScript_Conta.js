80f037a98ffaa1779edcc310ab8de26c7eef6247
Pedro Agra Sabino <pedros@l3.com.br>
2020-12-16 22:37:48 -0300

    initial commit

---
+++ Academia.PowerPlatform/Academia.PowerPlatform.JavaScript/JS/JavaScript_Conta.js
@@ -0,0 +1,108 @@
+﻿function HelloWorld(executionContext) {
+
+	var formContext = executionContext.getFormContext();
+
+	//console.log(executionContext);
+
+	//var name = formContext.getAttribute('name').getValue();
+
+	//console.log(name);
+
+	//formContext.getAttribute('accountnumber').setValue(name + "_12345678");
+
+
+	//alert('TESTANDO FIDDLER');
+
+	//var tickersymbol = formContext.getAttribute('tickersymbol').getValue();
+
+	//if (tickersymbol === 'TEL') {
+
+	//	formContext.getControl('accountnumber').setDisabled(true);
+
+	//	formContext.getControl('telephone1').setVisible(true);
+	//	formContext.getControl('fax').setVisible(false);
+	//	formContext.getControl('websiteurl').setVisible(false);
+	//}
+	//else if (tickersymbol === 'FAX') {
+
+	//	formContext.getControl('telephone1').setVisible(false);
+	//	formContext.getControl('fax').setVisible(true);
+	//	formContext.getControl('websiteurl').setVisible(false);
+	//}
+	//else {
+
+	//	formContext.getControl('telephone1').setVisible(false);
+	//	formContext.getControl('fax').setVisible(false);
+	//	formContext.getControl('websiteurl').setVisible(true);
+	//}
+
+	//Xrm.WebApi.retrieveMultipleRecords("account", "?$select=name&$top=3").then(
+
+	//	function success(result) {
+	//		for (var i = 0; i < result.entities.length; i++) {
+	//			console.log(result.entities[i]);
+	//		}
+	//		// perform additional operations on retrieved records
+	//	},
+
+	//	function (error) {
+	//		console.log(error.message);
+	//		// handle error conditions
+	//	}
+	//);
+
+	//var data =
+	//{
+	//	"name": "Sample Account",
+	//	"creditonhold": false,
+	//	"address1_latitude": 47.639583,
+	//	"description": "This is the description of the sample account",
+	//	"revenue": 5000000,
+	//	"accountcategorycode": 1
+	//}
+
+	//// create account record
+	//Xrm.WebApi.createRecord("account", data).then(
+
+	//	function success(result) {
+	//		console.log("Account created with ID: " + result.id);
+	//		// perform operations on record creation
+	//	},
+	//	function (error) {
+	//		console.log(error.message);
+	//		// handle error conditions
+	//	}
+	//);
+	//var formOptions = {
+	//	"entityName": "contact"
+	//};
+
+	//Xrm.Navigation.openForm(formOptions);
+
+	formContext.ui.setFormNotification("sessão de treinamento de power platform...", "ERROR", "1");
+	formContext.ui.setFormNotification("sessão de treinamento de power platform...", "WARNING", "2");
+	formContext.ui.setFormNotification("sessão de treinamento de power platform...", "INFO", "3");
+}
+
+
+function OpenCurrentSystemUser(executionContext) {
+
+	debugger;
+
+	console.log(executionContext);
+
+	Xrm.Navigation.openAlertDialog({
+		"text": "Abrindo o registro do usuário logado." 
+	})
+
+	var userSettings = Xrm.Utility.getGlobalContext().userSettings;
+	var userId = userSettings.userId;
+
+	console.log(userId);
+
+	Xrm.Navigation.openForm({
+
+		"entityName": "systemuser",
+		"entityId": userId
+	});
+}
---
+++ Academia.PowerPlatform/Academia.PowerPlatform/AtualizarCampoRelacionado.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academia.PowerPlatform
+{
+	public class AtualizarCampoRelacionado : IPlugin
+	{
+		public void Execute(IServiceProvider serviceProvider)
+		{
+			// Instancia do servico e 
+			IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+			IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+			IOrganizationService service = (IOrganizationService)serviceFactory.CreateOrganizationService(context.UserId);
+
+			// Log de plugins - Trace
+
+			ITracingService tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+			tracing.Trace($"Forma de registrar um log no CDS para monitorar a execução do plug-in. {DateTime.Now}");
+
+			// ID da Conta
+
+			Entity accountCreated = (Entity)context.InputParameters["Target"];
+
+			// 1 - QueryExpresion
+
+			QueryExpression qe = new QueryExpression("account");
+			qe.ColumnSet.AddColumns("websiteurl");
+			qe.Criteria.AddCondition("accountid", ConditionOperator.Equal, accountCreated.Id);
+
+			var resultSet_1 = service.RetrieveMultiple(qe);
+
+			// 2 - FetchXml
+
+			var fetchXml = "" +
+			"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>" +
+			  "<entity name='account'>" +
+				"<attribute name='name' /> " +
+				"<attribute name='primarycontactid' /> " +
+				"<attribute name='telephone1' /> " +
+				"<attribute name='accountid' /> " +
+				"<order attribute='name' descending='false' /> " +
+				"<filter type='and'> " +
+				  "<condition attribute='accountid' operator='eq' uiname='TESTING COMPANY' uitype='account' value='" + accountCreated.Id + "' />" +
+				"</filter> " +
+			  "</entity> " +
+			"</fetch>";
+
+			var resutSet_2 = service.RetrieveMultiple(new FetchExpression(fetchXml));
+
+			// 2 - Retrieve
+
+			var accountRetrieved = service.Retrieve("account", accountCreated.Id, new ColumnSet("websiteurl"));
+
+			Entity entity = new Entity("contact");
+			entity["fullname"] = "NOME FIXO";
+			entity["emailaddress1"] = accountRetrieved.GetAttributeValue<string>("websiteurl");
+			entity["parentcustomerid"] = new EntityReference(accountCreated.LogicalName, accountCreated.Id);
+
+			var contatoId = service.Create(entity);
+
+
+			// Update
+
+			var updateContato = new Entity("contact", contatoId);
+			updateContato["fullname"] = "Qualquer outra coisa";
+
+			service.Update(updateContato);
+
+			// Delete
+
+			service.Delete("contact", contatoId);
+
+			// Execute - ReviseQuote
+
+			var request = new ReviseQuoteRequest();
+			request.ColumnSet = new ColumnSet(true);
+			request.QuoteId = new Guid("df8004eb-303f-eb11-a813-000d3ac058c3");
+
+			var response = (ReviseQuoteResponse)service.Execute(request);
+		}
+	}
+}
